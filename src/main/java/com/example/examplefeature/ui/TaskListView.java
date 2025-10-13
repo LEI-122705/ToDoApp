5b7c01806ad58a2311c4168794a4af1d5b333e55
cmsca3 <cmsca3@iscte-iul.pt>
2025-10-13 10:47:05 +0100

    correcao data pdfs

--- src/main/java/com/example/examplefeature/ui/TaskListView.java
+++ src/main/java/com/example/examplefeature/ui/TaskListView.java
@@ -109,7 +109,7 @@ class TaskListView extends Main {
                 List l= new List(List.UNORDERED); //ORDERED da numbered lista
 
                 //guardar todos os elementos
-                DateTimeFormatter dateForm = DateTimeFormatter.ofLocalizedDateTime(FormatStyle.MEDIUM).withLocale(getLocale());//para formatar datas corretamente
+                DateTimeFormatter dateForm = DateTimeFormatter.ofLocalizedDate(FormatStyle.MEDIUM).withLocale(getLocale());//para formatar datas corretamente
 
                 var tasks= taskService.listAll(); //assim nao tenho de converter a tabela, faz automaticamente :/
                 for(Task t: tasks){
